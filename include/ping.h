279aba39b0ffab4f714f672d70df4a5f33185fb0
wise-Kaas <kamilaliev@hotmail.com>
2020-12-03 18:33:07 +0300

    added structure for icmp packet was able to retrieve incoming packet source address

--- include/ping.h
+++ include/ping.h
@@ -5,4 +5,21 @@
 #include <stdio.h>
 //END
 
+typedef struct s_icmp_hdr {
+    u_int8_t    type; // message type, 0 - echo reply, 8 - echo request
+    u_int8_t    code; // used for error code
+    u_int16_t   chksum; // checksum of header plus data
+    u_int16_t   id; // identifier used to match requests/replies
+    u_int16_t   seq; // used to identify the sequence if more than one is sent
+}   t_icmp_hdr;
+
+typedef struct s_icmp_data {
+    u_int32_t   content;
+}   t_icmp_data;
+
+typedef struct s_icmp_pack {
+    t_icmp_hdr  header;
+    t_icmp_data data;
+}   t_icmp_pack;
+
 #endif
--- src/ft_ping.c
+++ src/ft_ping.c
@@ -13,28 +13,40 @@
 
 int	main()
 {
-    int sfd, value;
+    int sfd;
+    char on;
     struct sockaddr_in sin;
     char ip4[INET_ADDRSTRLEN];
     char buf[400];
+    t_icmp_pack *packet;
 
-    if ((sfd = socket(AF_INET, SOCK_RAW, IPPROTO_RAW)) < 0)
+    packet = (t_icmp_pack *)buf;
+    memset(packet, 0, sizeof(*packet));
+    packet->header.type = 8;
+    packet->header.id = 1;
+    packet->header.seq = 1;
+    packet->data.content = 15;
+    //memcpy(&(packet->data), "hello", strlen("hello"));
+    printf("size of packet - %ld\n", sizeof(packet));
+
+    if ((sfd = socket(AF_INET, SOCK_RAW, IPPROTO_ICMP)) < 0)
     {
         perror("socket() error");
         exit(1);
     }
 
     // tell kernel we gonna include IP header ourselves
-    value = 1;
-    if(setsockopt(sfd, IPPROTO_IP, IP_HDRINCL, &value, sizeof(value)) < 0)
-    {
-        perror("setsockopt() error");
-        exit(-1);
-    }
-    else
-        printf("setsockopt() is OK\n");
+    on = 1;
+//    if(setsockopt(sfd, IPPROTO_IP, IP_HDRINCL, &on, sizeof(on)) < 0)
+//    {
+//        perror("setsockopt() error");
+//        exit(-1);
+//    }
+//    else
+//        printf("setsockopt() is OK\n");
 
     // prepare info about destination host, that is address family and address itself
+    memset(&sin, 0, sizeof(sin));
     sin.sin_family = AF_INET;
 
     //convert human readable address form (with dots) to bit (network) form
@@ -47,13 +59,43 @@ int	main()
     printf("address is %s\n", ip4);
 
     ssize_t b;
-    if ((b = sendto(sfd, buf, sizeof(buf), 0, (struct sockaddr *)&sin, sizeof(sin))) < 0)
+    if ((b = sendto(sfd, buf, sizeof(*packet), 0, (struct sockaddr *)&sin, sizeof(sin))) < 0)
     {
         perror("sendto() error");
         exit(-1);
     } else {
         printf("sent - %ld\n", b);
     }
+    struct msghdr msg;
+    struct iovec io;
+    char content[400];
+    struct sockaddr_in rec_addr;
+    char control[400];
+    memset(content, 0, 400);
+    memset(&rec_addr, 0, sizeof(rec_addr));
+    memset(control, 0, 400);
+    memset(&msg, 0, sizeof(msg));
+    msg.msg_name = &rec_addr;
+    msg.msg_namelen = sizeof(rec_addr);
+    msg.msg_control = control;
+    msg.msg_controllen = 400;
+    io.iov_base = content;
+    io.iov_len = 400;
+    msg.msg_iov = &io;
+    msg.msg_iovlen = 1;
+
+    printf("size of sockaddr_in - %ld\n", sizeof(rec_addr));
+
+    if ((b = recvmsg(sfd, &msg, 0)) < 0 )
+    {
+        perror("revmsg() error");
+        exit(-1);
+    } else {
+        printf("received - %ld\n", b);
+    }
+
+    printf("message - %ud\n", *(u_int32_t *)&(io.iov_base));
+    printf("incoming address - %d\n", *((int*)&rec_addr.sin_addr));
 
     return (0);
 }
